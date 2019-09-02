9a26dc17033ea7a2a78057ff7f31c3fae38d0a18
barrymun <barrymun@tcd.ie>
2019-09-02 15:24:12 +0100

    [wip] download speed (attached to loop for average calc)

--- src/SpeedTest/Main.js
+++ src/SpeedTest/Main.js
@@ -19,10 +19,45 @@ class Main extends React.Component {
 
 
     async componentDidMount() {
-        this.getDownloadSpeed();
+        this.sumDownloadSpeed();
         // this.sumPing();
-
     }
+
+    sumDownloadSpeed = async () => {
+        console.log('A')
+        await this.getDownloadSpeed(res => console.log(res));
+        console.log('B')
+        // for (let index = 0; index < 20; index++) {
+        //     console.log('A')
+        //     await this.getDownloadSpeed(res => console.log(res));
+        //     console.log('B')
+        //     await sleep(50);
+        // }
+    };
+
+    getDownloadSpeed = async callback => {
+        let imageAddr = "https://hdqwalls.com/download/chicago-city-waterfall-8k-x1-7680x4320.jpg";
+        let downloadSize = 6937258;  // bytes
+        let startTime, endTime;
+        let download = new Image();
+
+        download.onload = function () {
+            endTime = (new Date()).getTime();
+            let duration = (endTime - startTime) / 1000;
+            let bitsLoaded = downloadSize * 8;
+            let speedBps = (bitsLoaded / duration).toFixed(2);
+            let speedKbps = (speedBps / 1024).toFixed(2);
+            let speedMbps = (speedKbps / 1024).toFixed(2);
+            // console.log({speedBps, speedKbps, speedMbps})
+            callback({speedBps, speedKbps, speedMbps});
+        };
+
+        download.onerror = function (err, msg) {
+        };
+
+        startTime = (new Date()).getTime();
+        download.src = `${imageAddr}?cacheCleaner=${startTime}`;
+    };
 
     sumPing = async () => {
         for (let index = 0; index < 100; index++) {
@@ -33,80 +68,11 @@ class Main extends React.Component {
         }
     };
 
-    getDownloadSpeed = () => {
-        // var imageAddr = "/test.jpg";
-        // var imageAddr = "https://i.pinimg.com/originals/5c/f7/39/5cf7390adae7111d1ccc7b0f6625fc9d.jpg";
-        var imageAddr = "https://hdqwalls.com/download/chicago-city-waterfall-8k-x1-7680x4320.jpg";
-        // var downloadSize = 4995374; //bytes
-        var downloadSize = 6937258;  // bytes
-
-        function ShowProgressMessage(msg) {
-            if (console) {
-                if (typeof msg == "string") {
-                    console.log(msg);
-                } else {
-                    for (var i = 0; i < msg.length; i++) {
-                        console.log(msg[i]);
-                    }
-                }
-            }
-
-            var oProgress = document.getElementById("progress");
-            if (oProgress) {
-                var actualHTML = (typeof msg == "string") ? msg : msg.join("<br />");
-                oProgress.innerHTML = actualHTML;
-            }
-        }
-
-        function InitiateSpeedDetection() {
-            // ShowProgressMessage("Loading the image, please wait...");
-            window.setTimeout(MeasureConnectionSpeed, 1);
-        }
-
-        if (window.addEventListener) {
-            window.addEventListener('load', InitiateSpeedDetection, false);
-        } else if (window.attachEvent) {
-            window.attachEvent('onload', InitiateSpeedDetection);
-        }
-
-        function MeasureConnectionSpeed() {
-            var startTime, endTime;
-            var download = new Image();
-            download.onload = function () {
-                endTime = (new Date()).getTime();
-                showResults();
-            };
-
-            download.onerror = function (err, msg) {
-                // ShowProgressMessage("Invalid image, or error downloading");
-            };
-
-            startTime = (new Date()).getTime();
-            var cacheCleaner = "?cacheCleaner=" + startTime;
-            download.src = `${imageAddr}${cacheCleaner}`;
-
-            function showResults() {
-                var duration = (endTime - startTime) / 1000;
-                var bitsLoaded = downloadSize * 8;
-                var speedBps = (bitsLoaded / duration).toFixed(2);
-                var speedKbps = (speedBps / 1024).toFixed(2);
-                var speedMbps = (speedKbps / 1024).toFixed(2);
-                console.log({speedBps, speedKbps, speedMbps})
-                // ShowProgressMessage([
-                //     "Your connection speed is:",
-                //     speedBps + " bps",
-                //     speedKbps + " kbps",
-                //     speedMbps + " Mbps"
-                // ]);
-            }
-        }
-    };
-
     /**
      *
      */
     getPing = async (host, callback) => {
-        let started = new Date().getTime();
+        let startTime = new Date().getTime();
         let xhr = new XMLHttpRequest();
 
         xhr.open("GET", `${host}?cacheCleaner=${uuid()}`, true);
@@ -118,8 +84,8 @@ class Main extends React.Component {
             callback(null, 0);
         };
         xhr.onerror = function () {
-            let ended = new Date().getTime();
-            let ping = ended - started;
+            let endTime = new Date().getTime();
+            let ping = endTime - startTime;
             callback(ping);
         };
         xhr.send();
